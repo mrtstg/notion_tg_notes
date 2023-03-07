82de6fe1dbf87e82a86dfd84656fdb40f5168a76
mrtstg <russiankurwacsgo@gmail.com>
2023-03-07 17:04:57 +0300

    added notes creating

--- src/api/api.py
+++ src/api/api.py
@@ -1,7 +1,44 @@
 from . import API_URL
 from .structs import NotionDatabase
+from .properties import (
+    CheckboxPageProperty,
+    DatePageProperty,
+    SelectPageProperty,
+    MultiSelectPageProperty,
+    TitlePageProperty,
+)
 import aiohttp
 import asyncio
+
+
+class NotionNote:
+    title: TitlePageProperty
+    remind: CheckboxPageProperty
+    date: DatePageProperty
+    importance: SelectPageProperty
+    progress: SelectPageProperty
+    category: MultiSelectPageProperty
+
+    def __init__(self):
+        self.title = TitlePageProperty("Title")
+        self.remind = CheckboxPageProperty("Remind")
+        self.date = DatePageProperty("Date")
+        self.importance = SelectPageProperty("Importance")
+        self.progress = SelectPageProperty("Progress")
+        self.category = MultiSelectPageProperty("Category")
+
+    def get_json(self) -> dict:
+        data = {}
+        for i in [
+            self.title,
+            self.remind,
+            self.date,
+            self.importance,
+            self.progress,
+            self.category,
+        ]:
+            data.update(i.get_json())
+        return data
 
 
 class NotionApi:
@@ -40,3 +77,19 @@ class NotionApi:
         resp.raise_for_status()
         data = await resp.json()
         return NotionDatabase(data)
+
+    async def create_note(self, note: NotionNote, database_id: str) -> dict:
+        assert self.client is not None
+        resp = await self.client.post(
+            "/v1/pages",
+            json={
+                "parent": {"database_id": database_id},
+                "properties": note.get_json(),
+            },
+        )
+        resp.raise_for_status()
+        return await resp.json()
+
+    def __del__(self):
+        if self.client is not None:
+            asyncio.run(self.client.close())
