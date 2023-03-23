8540861e96f78578fec87acd6872d054d44a47b3
mrtstg <russiankurwacsgo@gmail.com>
2023-03-23 23:09:27 +0300

    added config dependent varians and almost done note creating:

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 
 class FileConfig:
@@ -6,9 +7,17 @@ class FileConfig:
     tg_token: str
     db_id: str
     _path: str
+    importance_values: list[str]
+    remind_values: list[str]
+    progress_values: list[str]
+    categories_values: list[str]
 
     def __init__(self, path: str):
         self._path = path
         with open(path, "r", encoding="utf-8") as file:
             data = yaml.safe_load(file)
             self.__dict__.update(**data)
+
+
+def get_config() -> FileConfig:
+    return FileConfig(os.environ.get("CONFIG_FILE", "config.yaml"))
--- src/routes/note_creating.py
+++ src/routes/note_creating.py
@@ -1,16 +1,14 @@
+from typing import Callable
 from aiogram import F, Router, types
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from . import make_row_keyboard
+from config import get_config
 
 router = Router()
-
-available_importance = ["Важно", "Неважно", "Срочно"]
-available_remind_values = ["Да", "Нет"]
-available_progress = ["Не начато", "Начато", "Завершено"]
-available_categories = ["Работа", "Дом", "Учеба", "Преподавательство"]
+CONFIG = get_config()
 
 
 class NoteCreatingStage(StatesGroup):
@@ -20,6 +18,7 @@ class NoteCreatingStage(StatesGroup):
 @router.message(Command("note"))
 async def create_note(message: Message, state: FSMContext):
     await message.answer("Введите заголовок заметки: ")
+    await state.update_data(categories=[])
     await state.set_state(NoteCreatingStage.TITLE)
 
 
@@ -28,15 +27,63 @@ async def set_note_title(message: Message, state: FSMContext):
     await state.update_data(title=message.text)
     await message.answer(
         text="Выберите уровень важности:",
-        reply_markup=make_row_keyboard(available_importance),
+        reply_markup=make_row_keyboard(CONFIG.importance_values),
     )
     await state.set_state(NoteCreatingStage.IMPORTANCE)
 
 
-@router.message(NoteCreatingStage.IMPORTANCE, F.text.in_(available_importance))
+@router.message(NoteCreatingStage.IMPORTANCE, F.text.in_(CONFIG.importance_values))
 async def set_note_importance(message: Message, state: FSMContext):
     await state.update_data(importance=message.text)
     await message.answer(
-        str(await state.get_data()), reply_markup=types.ReplyKeyboardRemove()
+        text="Выберите начальный прогресс заметки:",
+        reply_markup=make_row_keyboard(CONFIG.progress_values),
     )
-    await state.clear()
+    await state.set_state(NoteCreatingStage.PROGESS)
+
+
+@router.message(NoteCreatingStage.PROGESS, F.text.in_(CONFIG.progress_values))
+async def set_note_progress(message: Message, state: FSMContext):
+    await state.update_data(progress=message.text)
+    await message.answer(
+        text="Напомнить о наличии заметки?",
+        reply_markup=make_row_keyboard(["Да", "Нет"]),
+    )
+    await state.set_state(NoteCreatingStage.REMIND)
+
+
+@router.message(NoteCreatingStage.REMIND, F.text.in_(["Да", "Нет"]))
+async def set_note_remind(message: Message, state: FSMContext):
+    await state.update_data(remind=message.text == "Да")
+    await message.answer(
+        text="Введите категории заметки.",
+        reply_markup=make_row_keyboard(CONFIG.categories_values + ["done"]),
+    )
+    await state.set_state(NoteCreatingStage.CATEGORIES)
+
+
+@router.message(NoteCreatingStage.CATEGORIES, F.text == "done")
+async def end_note_creation(message: Message, state: FSMContext):
+    await message.answer(
+        text=str(await state.get_data()), reply_markup=types.ReplyKeyboardRemove()  # type: ignore
+    )
+    await state.set_state(None)
+
+
+@router.message(NoteCreatingStage.CATEGORIES, F.text.in_(CONFIG.categories_values))
+async def note_category_action(message: Message, state: FSMContext):
+    assert message.text is not None
+    generate_categories: Callable[
+        [list[str]], str
+    ] = lambda x: "Текущие категории: " + ",".join(x)
+    cat_list: list[str] = (await state.get_data())["categories"]
+    if message.text in cat_list:
+        cat_list.pop(cat_list.index(message.text))
+        await message.answer(
+            text="Категория удалена!\n" + generate_categories(cat_list)
+        )
+    else:
+        cat_list.append(message.text)
+        await message.answer(
+            text="Категория добавлена!\n" + generate_categories(cat_list)
+        )
