5b0fcc957c65a454339370d4606cd66a502d4d24
mrtstg <russiankurwacsgo@gmail.com>
2023-06-13 12:31:30 +0300

    removed numpy from dependencies

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,11 +1,16 @@
+from typing import Generator
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from numpy import array_split
+
+
+def divide_chunks(lst: list, n: int) -> Generator[list[list], None, None]:
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]
 
 
 def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
     buttons = [KeyboardButton(text=item) for item in items]
     markup = ReplyKeyboardMarkup(keyboard=[], resize_keyboard=True)
-    for row in array_split(buttons, 3):  # type: ignore
-        markup.keyboard.append(list(row))
+    for row in divide_chunks(buttons, 4):
+        markup.keyboard.append(list(row))  # type: ignore
 
     return markup
